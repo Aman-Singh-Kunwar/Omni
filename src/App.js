ec8234cb1cfa160edc73cd3ee7b20a2493a76203
Aman Singh Kunwar <amansinghkunwar07@gmail.com>
2025-09-19 22:25:07 +0530

    trying

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
 import React from 'react';
-// 1. Import HashRouter instead of BrowserRouter
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';  // Change to HashRouter
 import LandingPage from './components/landing/LandingPage';
 import CustomerDashboard from './components/customer/CustomerDashboard';
 import BrokerDashboard from './components/broker/BrokerDashboard';
 import WorkerDashboard from './components/worker/WorkerDashboard';
-import { AuthProvider } from './context/AuthContext';
 import './App.css';
 
 function App() {
   return (
-    <AuthProvider>
-      {/* 2. Use the imported Router (which is now HashRouter) */}
-      <Router>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/customer" element={<CustomerDashboard />} />
-            <Route path="/broker" element={<BrokerDashboard />} />
-            <Route path="/worker" element={<WorkerDashboard />} />
-            {/* A fallback route can be useful */}
-            <Route path="*" element={<LandingPage />} />
-          </Routes>
-        </div>
-      </Router>
-    </AuthProvider>
+    <HashRouter>  {/* Wrap with HashRouter */}
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/customer" element={<CustomerDashboard />} />
+        <Route path="/broker" element={<BrokerDashboard />} />
+        <Route path="/worker" element={<WorkerDashboard />} />
+        <Route path="*" element={<div>404 - Page Not Found</div>} />
+      </Routes>
+    </HashRouter>
   );
 }
 
 export default App;
-
